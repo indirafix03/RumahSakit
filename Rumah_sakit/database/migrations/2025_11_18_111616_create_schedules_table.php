417892331df14f15746f1fb36710f1037f13e8ac
indirafix03 <indiraramayani14@gmail.com>
2025-11-18 20:28:21 +0800

    setup database

---
+++ Rumah_sakit/database/migrations/2025_11_18_111616_create_schedules_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('dokter_id')->constrained('users')->onDelete('cascade');
+            $table->enum('hari', ['senin', 'selasa', 'rabu', 'kamis', 'jumat', 'sabtu', 'minggu']);
+            $table->time('jam_mulai');
+            $table->integer('durasi')->default(30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('schedules');
+    }
+};
---
+++ Rumah_sakit/database/migrations/2025_11_18_120944_create_resep_obat_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('resep_obat', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('medical-record_id')->constrained('medical_records')->onDelete('cascade');
+            $table->foreignId('medicine_id')->constrained('medicines')->onDelete('cascade');
+            $table->integer('jumlah');
+            $table->text('aturan_pakai')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('resep_obat');
+    }
+};
